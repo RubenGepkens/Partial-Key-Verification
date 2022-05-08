1119fc049587471b94a3345ec02284076f1e359a
RubenGepkens <71176363+RubenGepkens@users.noreply.github.com>
2022-05-08 22:32:52 +0200

    Added datagridview for storing keys and added menu options for keygeneration

--- PartialKeyGUI/frmPKV.Designer.cs
+++ PartialKeyGUI/frmPKV.Designer.cs
@@ -29,34 +29,243 @@ namespace PartialKeyGUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.btnGenerateKey = new System.Windows.Forms.Button();
+            this.cbxChecksum = new System.Windows.Forms.ComboBox();
+            this.cbxHash = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudSpacing = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudSubKeys = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSeed = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteRowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSpacing)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSubKeys)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnGenerateKey
             // 
-            this.button1.Location = new System.Drawing.Point(323, 130);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnGenerateKey.Location = new System.Drawing.Point(299, 119);
+            this.btnGenerateKey.Name = "btnGenerateKey";
+            this.btnGenerateKey.Size = new System.Drawing.Size(121, 23);
+            this.btnGenerateKey.TabIndex = 0;
+            this.btnGenerateKey.Text = "Generate key";
+            this.btnGenerateKey.UseVisualStyleBackColor = true;
+            this.btnGenerateKey.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // cbxChecksum
+            // 
+            this.cbxChecksum.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxChecksum.FormattingEnabled = true;
+            this.cbxChecksum.Location = new System.Drawing.Point(86, 65);
+            this.cbxChecksum.Name = "cbxChecksum";
+            this.cbxChecksum.Size = new System.Drawing.Size(121, 21);
+            this.cbxChecksum.TabIndex = 1;
+            // 
+            // cbxHash
+            // 
+            this.cbxHash.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxHash.FormattingEnabled = true;
+            this.cbxHash.Location = new System.Drawing.Point(86, 92);
+            this.cbxHash.Name = "cbxHash";
+            this.cbxHash.Size = new System.Drawing.Size(121, 21);
+            this.cbxHash.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Checksum:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hash:";
+            // 
+            // nudSpacing
+            // 
+            this.nudSpacing.Location = new System.Drawing.Point(299, 93);
+            this.nudSpacing.Name = "nudSpacing";
+            this.nudSpacing.Size = new System.Drawing.Size(120, 20);
+            this.nudSpacing.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(242, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Spacing:";
+            // 
+            // nudSubKeys
+            // 
+            this.nudSubKeys.Location = new System.Drawing.Point(299, 66);
+            this.nudSubKeys.Name = "nudSubKeys";
+            this.nudSubKeys.Size = new System.Drawing.Size(120, 20);
+            this.nudSubKeys.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(242, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Subkeys:";
+            // 
+            // txtSeed
+            // 
+            this.txtSeed.Location = new System.Drawing.Point(86, 40);
+            this.txtSeed.Name = "txtSeed";
+            this.txtSeed.Size = new System.Drawing.Size(333, 20);
+            this.txtSeed.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Seed:";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(86, 148);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(333, 20);
+            this.txtKey.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 151);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Key:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 205);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(543, 233);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.deleteRowToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.deleteAllToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(193, 98);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.copyToolStripMenuItem.Text = "Copy selected row key";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // deleteRowToolStripMenuItem
+            // 
+            this.deleteRowToolStripMenuItem.Name = "deleteRowToolStripMenuItem";
+            this.deleteRowToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.deleteRowToolStripMenuItem.Text = "Delete selected row(s)";
+            this.deleteRowToolStripMenuItem.Click += new System.EventHandler(this.deleteRowToolStripMenuItem_Click);
+            // 
+            // deleteAllToolStripMenuItem
+            // 
+            this.deleteAllToolStripMenuItem.Name = "deleteAllToolStripMenuItem";
+            this.deleteAllToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteAllToolStripMenuItem.Text = "Delete all";
+            this.deleteAllToolStripMenuItem.Click += new System.EventHandler(this.deleteAllToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // frmPKV
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(567, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSeed);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nudSubKeys);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudSpacing);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxHash);
+            this.Controls.Add(this.cbxChecksum);
+            this.Controls.Add(this.btnGenerateKey);
+            this.Name = "frmPKV";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmPKV_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudSpacing)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSubKeys)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGenerateKey;
+        private System.Windows.Forms.ComboBox cbxChecksum;
+        private System.Windows.Forms.ComboBox cbxHash;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudSpacing;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudSubKeys;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSeed;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteRowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem deleteAllToolStripMenuItem;
     }
 }
 
--- PartialKeyGUI/frmPKV.cs
+++ PartialKeyGUI/frmPKV.cs
@@ -16,6 +16,24 @@ namespace PartialKeyGUI
 {
     public partial class frmPKV : Form
     {
+        private enum pkvChecksum
+        {
+            Adler16,
+            Crc16,
+            CrcCcitt
+        }
+
+        private enum pkvHash
+        {
+            Crc32,
+            Fnv1A,
+            GeneralizedCrc,
+            Jenkins06,
+            Jenkins96,
+            OnaAtATime,
+            SuperFast
+        }
+
         public frmPKV()
         {
             InitializeComponent();
@@ -24,14 +42,133 @@ namespace PartialKeyGUI
         private void button1_Click(object sender, EventArgs e)
         {
 
-            var generator = new PartialKeyGenerator(new Adler16(), new Jenkins96(), new uint[] { 1 }) { Spacing = 5 };
-            var key = generator.Generate("bob@smith.com");
+            var generator = new PartialKeyGenerator(new Adler16(), new Jenkins96(), new uint[] { 1, 2 }) { Spacing = 5 };
+            var key = generator.Generate("henk");
 
             Console.WriteLine("{0}", key.ToString());
 
             var isValid = PartialKeyValidator.ValidateKey(new Adler16(), new Jenkins96(), key, 0, 1);
 
             Console.WriteLine("{0}", isValid);
+
+            IChecksum16 objChecksum;
+            IHash objHash;
+
+            switch (cbxChecksum.SelectedIndex)
+            {
+                case 0:
+                    objChecksum = new Adler16();
+                    break;
+                case 1:
+                    objChecksum = new Crc16();
+                    break;
+                case 2:
+                    objChecksum = new CrcCcitt();
+                    break;
+                default:
+                    objChecksum = new Adler16();
+                    break;
+            }
+
+            switch (cbxHash.SelectedIndex)
+            {
+                case 0:
+                    objHash = new Crc32();
+                    break;
+                case 1:
+                    objHash = new Fnv1A();
+                    break;
+                case 2:
+                    objHash = new GeneralizedCrc();
+                    break;
+                case 3:
+                    objHash = new Jenkins06(0);
+                    break;
+                case 4:
+                    objHash = new Jenkins96();
+                    break;
+                case 5:
+                    objHash = new OneAtATime();
+                    break;
+                case 6:
+                    objHash = new SuperFast();
+                    break;
+                default:
+                    objHash = new Jenkins96();
+                    break;
+            }
+
+            uint[] uInt = new uint[] { };
+            int intKeySize = (int)nudSubKeys.Value;            
+            List<uint> list = new List<uint>();
+
+            for (uint k=0; k<intKeySize; k++)
+            {
+                list.Add(k+1);
+            }
+
+            uInt = list.ToArray();
+
+            generator = new PartialKeyGenerator(objChecksum, objHash, uInt) { Spacing = (byte)nudSpacing.Value };
+            key = generator.Generate(txtSeed.Text);
+
+            txtKey.Text = key;
+
+            dataGridView1.Rows.Add(
+                cbxChecksum.SelectedItem,
+                cbxHash.SelectedItem,
+                txtSeed.Text,
+                nudSubKeys.Value.ToString(),
+                txtKey.Text);
+            dataGridView1.AutoResizeColumns();
+        }
+
+        private void frmPKV_Load(object sender, EventArgs e)
+        {
+            foreach (string s in Enum.GetNames(typeof(pkvChecksum)))
+            {
+                cbxChecksum.Items.Add(s);
+            }
+            
+            foreach (string s in Enum.GetNames(typeof(pkvHash)))
+            {
+                cbxHash.Items.Add(s);
+            }
+
+            cbxChecksum.SelectedIndex = 0;
+            cbxHash.SelectedIndex = 4;
+
+            nudSpacing.Value = 5;
+            nudSubKeys.Value = 2;
+
+            txtSeed.Text = "bob@smith.com";
+
+            dataGridView1.Columns.Add( "Checksum", "Checksum" );
+            dataGridView1.Columns.Add("Hash", "Hash");
+            dataGridView1.Columns.Add("Seed", "Seed");
+            dataGridView1.Columns.Add("Subkeys", "Subkeys");
+            dataGridView1.Columns.Add("Key", "Key");            
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
+            string key = dataGridView1.Rows[rowIndex].Cells["Key"].Value.ToString();
+
+            Clipboard.SetText(key);
+        }
+
+        private void deleteRowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.RemoveAt(row.Index);
+            }
+        }
+
+        private void deleteAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
         }
     }
 }
